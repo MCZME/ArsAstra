4cd3cfe8de6deac59e98f2a920e27bc611732a4a
MCZME <126310378+MCZME@users.noreply.github.com>
2026-01-03 15:10:30 +0800

    feat(element_profiles): 更新元素配置数据生成

--- src/main/java/com/github/mczme/arsastra/data/AAElementProfile.java
+++ src/main/java/com/github/mczme/arsastra/data/AAElementProfile.java
@@ -1,10 +1,10 @@
 package com.github.mczme.arsastra.data;
 
-import com.github.mczme.arsastra.ArsAstra;
 import com.github.mczme.arsastra.data.provider.ElementProfileProvider;
+import com.github.mczme.arsastra.registry.AAElements;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Items;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import org.joml.Vector2f;
 
@@ -19,9 +19,66 @@ public class AAElementProfile extends ElementProfileProvider {
 
     @Override
     protected void gather() {
-        basic("basic_items", 1000, basicBuilder()
-                .add(ResourceLocation.withDefaultNamespace("diamond"), new Vector2f(512.0f, 1286.0f), Map.of(
-                        ResourceLocation.fromNamespaceAndPath(ArsAstra.MODID, "order"), 20.0f
+        basic("vanilla_ingredients", 100, basicBuilder()
+                .add(Items.REDSTONE, new Vector2f(0.0f, 300.0f), Map.of(
+                        AAElements.ORDER, 18.0f,
+                        AAElements.STRUCTURE, 6.0f
+                ))
+                .add(Items.SUGAR, new Vector2f(-158.0f, 618.0f), Map.of(
+                        AAElements.ORDER, 10.0f,
+                        AAElements.GROWTH, 5.0f
+                ))
+                .add(Items.RABBIT_FOOT, new Vector2f(-58.0f, 923.0f), Map.of(
+                        AAElements.ORDER, 30.0f,
+                        AAElements.LIFE, 10.0f
+                ))
+                .add(Items.BLAZE_POWDER, new Vector2f(-488.0f, 769.0f), Map.of(
+                        AAElements.ORDER, 15.0f,
+                        AAElements.CORROSION, 10.0f
+                ))
+                .add(Items.GHAST_TEAR, new Vector2f(639.0f, 321.0f), Map.of(
+                        AAElements.LIFE, 35.0f,
+                        AAElements.ORDER, 15.0f
+                ))
+                .add(Items.GLISTERING_MELON_SLICE, new Vector2f(813.0f, 493.0f), Map.of(
+                        AAElements.LIFE, 20.0f,
+                        AAElements.ORDER, 5.0f
+                ))
+                .add(Items.PUFFERFISH, new Vector2f(484.0f, 566.0f), Map.of(
+                        AAElements.LIFE, 12.0f,
+                        AAElements.MATTER, 8.0f
+                ))
+                .add(Items.MAGMA_CREAM, new Vector2f(603.0f, 701.0f), Map.of(
+                        AAElements.MATTER, 15.0f,
+                        AAElements.ORDER, 10.0f
+                ))
+                .add(Items.PHANTOM_MEMBRANE, new Vector2f(-621.0f, 393.0f), Map.of(
+                        AAElements.ORDER, 18.0f,
+                        AAElements.DEATH, 7.0f
+                ))
+                .add(Items.GOLDEN_CARROT, new Vector2f(-719.0f, 250.0f), Map.of(
+                        AAElements.ORDER, 20.0f,
+                        AAElements.LIFE, 10.0f
+                ))
+                .add(Items.SPIDER_EYE, new Vector2f(468.0f, -537.0f), Map.of(
+                        AAElements.DECAY, 12.0f,
+                        AAElements.LIFE, 3.0f
+                ))
+                .add(Items.BREEZE_ROD, new Vector2f(-313.0f, -849.0f), Map.of(
+                        AAElements.ORDER, 35.0f,
+                        AAElements.STRUCTURE, 10.0f
+                ))
+                .add(Items.COBWEB, new Vector2f(-214.0f, -533.0f), Map.of(
+                        AAElements.STRUCTURE, 15.0f,
+                        AAElements.DECAY, 5.0f
+                ))
+                .add(Items.SLIME_BLOCK, new Vector2f(-283.0f, -706.0f), Map.of(
+                        AAElements.MATTER, 30.0f,
+                        AAElements.LIFE, 10.0f
+                ))
+                .add(Items.STONE, new Vector2f(214.0f, -533.0f), Map.of(
+                        AAElements.MATTER, 5.0f,
+                        AAElements.STRUCTURE, 5.0f
                 ))
         );
     }
--- src/main/java/com/github/mczme/arsastra/data/provider/ElementProfileProvider.java
+++ src/main/java/com/github/mczme/arsastra/data/provider/ElementProfileProvider.java
@@ -1,17 +1,24 @@
 package com.github.mczme.arsastra.data.provider;
 
 import com.github.mczme.arsastra.ArsAstra;
+import com.github.mczme.arsastra.core.element.Element;
 import com.github.mczme.arsastra.core.element.profile.ElementProfile;
 import com.github.mczme.arsastra.core.element.profile.definition.*;
+import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.PackType;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.ItemLike;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.common.data.JsonCodecProvider;
 import org.joml.Vector2f;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -55,8 +62,18 @@ public abstract class ElementProfileProvider extends JsonCodecProvider<Definitio
             return this;
         }
 
+        public BasicDefinitionBuilder add(ItemLike item, ElementProfile profile) {
+            return add(BuiltInRegistries.ITEM.getKey(item.asItem()), profile);
+        }
+
         public BasicDefinitionBuilder add(ResourceLocation item, Vector2f launchPoint, Map<ResourceLocation, Float> elements) {
             return add(item, new ElementProfile(launchPoint, elements));
+        }
+
+        public BasicDefinitionBuilder add(ItemLike item, Vector2f launchPoint, Map<? extends Holder<Element>, Float> elements) {
+            Map<ResourceLocation, Float> map = new HashMap<>();
+            elements.forEach((holder, value) -> map.put(holder.getKey().location(), value));
+            return add(BuiltInRegistries.ITEM.getKey(item.asItem()), launchPoint, map);
         }
 
         public BasicDefinition build() {
@@ -73,9 +90,37 @@ public abstract class ElementProfileProvider extends JsonCodecProvider<Definitio
             return this;
         }
 
+        public TemplateDefinitionBuilder data(Vector2f launchPoint, Map<? extends Holder<Element>, Float> elements) {
+            Map<ResourceLocation, Float> map = new HashMap<>();
+            elements.forEach((holder, value) -> map.put(holder.getKey().location(), value));
+            return data(new PartialProfile(Optional.ofNullable(launchPoint), Optional.of(map)));
+        }
+
         public TemplateDefinitionBuilder apply(TemplateApplyEntry entry) {
             this.apply.add(entry);
             return this;
+        }
+
+        public TemplateDefinitionBuilder apply(ItemLike item, Vector2f launchPoint, Map<? extends Holder<Element>, Float> elements) {
+            Map<ResourceLocation, Float> map = new HashMap<>();
+            elements.forEach((holder, value) -> map.put(holder.getKey().location(), value));
+            return apply(new TemplateApplyEntry(
+                    Optional.of(BuiltInRegistries.ITEM.getKey(item.asItem())),
+                    Optional.empty(),
+                    Optional.ofNullable(launchPoint),
+                    map
+            ));
+        }
+
+        public TemplateDefinitionBuilder apply(TagKey<Item> tag, Vector2f launchPoint, Map<? extends Holder<Element>, Float> elements) {
+            Map<ResourceLocation, Float> map = new HashMap<>();
+            elements.forEach((holder, value) -> map.put(holder.getKey().location(), value));
+            return apply(new TemplateApplyEntry(
+                    Optional.empty(),
+                    Optional.of(tag.location()),
+                    Optional.ofNullable(launchPoint),
+                    map
+            ));
         }
 
         public TemplateDefinition build() {
@@ -90,6 +135,20 @@ public abstract class ElementProfileProvider extends JsonCodecProvider<Definitio
         public RandomDefinitionBuilder apply(RandomApplyEntry entry) {
             this.apply.add(entry);
             return this;
+        }
+
+        public RandomDefinitionBuilder apply(ItemLike item) {
+            return apply(new RandomApplyEntry(
+                    Optional.of(BuiltInRegistries.ITEM.getKey(item.asItem())),
+                    Optional.empty()
+            ));
+        }
+
+        public RandomDefinitionBuilder apply(TagKey<Item> tag) {
+            return apply(new RandomApplyEntry(
+                    Optional.empty(),
+                    Optional.of(tag.location())
+            ));
         }
 
         public RandomDefinitionBuilder rules(RandomRules rules) {
